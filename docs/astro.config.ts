b254a70bd3ce2de05bdca8203e7d297497b8691a
trueberryless <trueberryless+github@gmail.com>
2025-03-20 21:11:29 +0100

    add links validator

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -2,6 +2,7 @@ import node from "@astrojs/node";
 import starlight from "@astrojs/starlight";
 import starlightPluginsDocsComponents from "@trueberryless-org/starlight-plugins-docs-components";
 import { defineConfig } from "astro/config";
+import starlightLinksValidator from "starlight-links-validator";
 import starlightPluginShowLatestVersion from "starlight-plugin-show-latest-version";
 import starlightTocOverviewCustomizer from "starlight-toc-overview-customizer";
 
@@ -13,6 +14,7 @@ export default defineConfig({
           "https://github.com/trueberryless-org/starlight-toc-overview-customizer/edit/main/docs/",
       },
       plugins: [
+        starlightLinksValidator(),
         starlightTocOverviewCustomizer({
           overviewTitle: "Back to top",
         }),
